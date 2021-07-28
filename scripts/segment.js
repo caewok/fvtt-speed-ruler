23fd8f3b24f562ad3a1731a2c0f86e7fa89a4742
Michael Enion <caewok_2000@yahoo.com>
2021-07-28 12:54:32 -0700

    Add call to speed provider to get color / range for token.

--- scripts/segment.js
+++ scripts/segment.js
@@ -1,6 +1,7 @@
 import { getSetting } from "./settings.js";
 import { log } from "./module.js";
-
+import { getRangesFromSpeedProvider, 
+         getUnreachableColorFromSpeedProvider } from "./api.js";
 /*
  * For a given position, return the color for the ruler highlight.
  * @param {Object} position Object with x and y indicating the pixels at the grid position.
@@ -12,18 +13,35 @@ export function speedRulerColorForPosition(wrapped, position) {
   // TO-DO: Move to Segment Property setup
   //        Store the token info, such as speed, speed multiplier
   if(!getSetting("speed-ruler-active")) return wrapped(position);
-  
+
   const starting_token = this.ruler._getMovementToken();
   if(!starting_token) return wrapped(position);
 
+  // Don't apply colors if the current user does not have at least observer permissions
+  if(token.actor.permission < CONST.ENTITY_PERMISSIONS.OBSERVER) {
+    return wrapped(position);
+  }
+
+  const ranges = getRangesFromSpeedProvider(token);
+  log(`token ${token.id} ranges`, ranges);
+  if(ranges.length === 0) return wrapped(position);
+  
   const distance = this.totalPriorDistance + this.measureDistance(position);
+  const currentRange = ranges.reduce((minRange, currentRange) => {
+		if (distance <= currentRange.range && currentRange.range < minRange.range)
+			return currentRange
+		return minRange
+	}, {range: Infinity, color: getUnreachableColorFromSpeedProvider()})
+
   
   // TO-DO: Pull info from token
-  log(`distance is ${distance}`);
-  const color = distance <= 30 ? 0x00FF00 :
-                (distance > 30 && distance <= 60) ? 0xFFFF00 :
-                0xFF0000;
+  log(`distance is ${distance}; color chosen is ${currentRange?.color}`);
+//   const color = distance <= 30 ? 0x00FF00 :
+//                 (distance > 30 && distance <= 60) ? 0xFFFF00 :
+//                 0xFF0000;
 
-  return color;
+  return currentRange.color;
 }
+
+
  
